f1a151ff3d440e1b9ebc497557155e6e7b614b9c
WrenRaming <lraming@asu.edu>
2024-08-05 09:59:07 -0600

    V3 (corresponding with notebook)  of wavelet mesh analysis

--- pytRIBS/mesh/wave_mesh.py
+++ pytRIBS/mesh/wave_mesh.py
@@ -4,107 +4,36 @@ import numpy as np
 import rasterio
 import pywt
 import pyvista as pv
-import matplotlib.pyplot as plt
+from affine import Affine
 import rasterio
 import numpy as np
+from scipy.interpolate import griddata
 from rasterio.transform import from_origin
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from scipy.spatial import Delaunay
 
 
-class WaveCell:
-    def __init__(self, ul_x, ul_y, dx, dy, level):
-        self.ul_x = ul_x
-        self.ul_y = ul_y
-        self.dx = dx
-        self.dy = dy
-        self.children = []
-        self.del_coeff = None
-        self.level = level
-
-    def subdivide(self, sig_masks_list, glob_min_x, glob_max_y):
-
-        self.get_del_coeff(sig_masks_list, glob_min_x, glob_max_y)
-
-        if self.level > 0 and self.del_coeff:
-
-            half_dx = self.dx / 2  # do these need to be updated with units???
-            half_dy = self.dy / 2
-            next_level = self.level - 1
-
-            # Create four children WaveCells at the next finer resolution
-            self.children = [
-                WaveCell(self.ul_x, self.ul_y, half_dx, half_dy, next_level),
-                WaveCell(self.ul_x + half_dx, self.ul_y, half_dx, half_dy, next_level),
-                WaveCell(self.ul_x, self.ul_y - half_dy, half_dx, half_dy, next_level),
-                WaveCell(self.ul_x + half_dx, self.ul_y - half_dy, half_dx, half_dy, next_level)
-            ]
-
-            # Recursively subdivide each child
-            for child in self.children:
-                child.subdivide(sig_masks_list, glob_min_x, glob_max_y)
-
-    def get_del_coeff(self, sig_masks_list, glob_min_x, glob_max_y):
-        if self.level == 0:
-            self.del_coeff = False
-        else:
-            sig_mask = sig_masks_list[self.level - 1]
-            col = int((self.ul_x - glob_min_x) / self.dx)
-            row = int((self.ul_y - glob_max_y) / self.dx)
-
-            self.del_coeff = sig_mask[row, col]
-
-
 class WaveletTree:
     def __init__(self, path_to_raster, maxlevel=None):
+        self.normalizing_coeff = None
         self.raster = path_to_raster
         self.maxlevel = maxlevel
         self.extract_raster_and_wavelet_info()
-        self.root_cell = WaveCell(self.bounds.left, self.bounds.top, self.width * self.transform[0].a,
-                                  self.height * self.transform[0].e, self.maxlevel)
 
     def extract_raster_and_wavelet_info(self):
         with rasterio.open(self.raster) as src:
-            data = src.read(1)  # Read the first band
-            transform = src.transform
-            bounds = src.bounds
-            width = src.width
-            height = src.height
+            self.data = src.read(1)  # Read the first band
+            self.transform = src.transform
+            self.bounds = src.bounds
+            self.width = src.width
+            self.height = src.height
 
-        self.dem_original = {
-            'data': data,
-            'transform': transform,
-            'bounds': bounds,
-            'width': width,
-            'height': height
-        }
-
-        self.data, self.width, self.height = self.pad_data_2d(data)
-
-        new_transform = from_origin(bounds.left, bounds.top, transform.a, transform.e)
-        self.bounds = rasterio.coords.BoundingBox(
-            bounds.left,
-            bounds.top + self.height * transform.e,
-            bounds.left + self.width * transform.a,
-            bounds.top
-        )
-
-        self.transform = new_transform,
-        self.wavelet_packet = pywt.WaveletPacket2D(data=self.data, wavelet='db1', mode='periodic',
+        self.wavelet_packet = pywt.WaveletPacket2D(data=self.data, wavelet='db1',
                                                    maxlevel=self.maxlevel)
-
         # update maxlevel incase it's none
         self.maxlevel = self.wavelet_packet.maxlevel
         self.normalizing_coeff = self.find_max_average_coeffs()
-
-    def pad_data_2d(self, data):
-        rows, cols = data.shape
-        padded_rows = 2 ** math.ceil(math.log2(rows))
-        padded_cols = 2 ** math.ceil(math.log2(cols))
-        padded_data = np.pad(data, ((0, padded_rows - rows), (0, padded_cols - cols)), mode='edge')
-
-        return padded_data, padded_cols, padded_rows
 
     def get_extent(self):
 
@@ -113,7 +42,7 @@ class WaveletTree:
         y_min = self.bounds.bottom
         y_max = self.bounds.top
 
-        return (x_min, x_max, y_min, y_max)
+        return x_min, x_max, y_min, y_max
 
     def find_max_average_coeffs(self):
         max_avg_coeffs_per_level = []
@@ -157,109 +86,169 @@ class WaveletTree:
                                  for x in range(0, r)])
 
             sig_masks_list.append(sig_mask)
-            thresh_exp - 1
 
-        # sig_masks_list.reverse()  # so it starts from coarsest and goes to finest
+            thresh_exp += -1
 
         return sig_masks_list
 
-    def fill_tree(self, sig_masks_list):
-        xmin = self.bounds.left
-        ymax = self.bounds.top
-        self.root_cell.subdivide(sig_masks_list, xmin, ymax)
+    def extract_points_from_significant_details(self, threshold):
 
-    def plot_cells(self, with_data=True):
-        fig, ax = plt.subplots()
-        if with_data:
-            ax.imshow(self.data, extent=self.get_extent())
+        def rowcol_to_xy(row, col, dx, dy):
+            x = (self.bounds.left + col * dx)  # + dx / 2  # center
+            y = (self.bounds.top - row * dy)  # + dy / 2  # center
 
-        def plot_cell(ax, cell, color='blue'):
+            return x, y
 
-            rect = patches.Rectangle(
-                (cell.ul_x, cell.ul_y - cell.dy), cell.dx, cell.dy,
-                linewidth=1, edgecolor=color, facecolor='none'
-            )
-            ax.add_patch(rect)
+        sig_masks_list = self.compute_normalized_coefficients(threshold)
+        centers = []
 
-            for child in cell.children:
-                plot_cell(ax, child, color='red')
+        for level in range(1, self.maxlevel + 1):
+            sig_mask = sig_masks_list[level - 1]
+            r, c = np.shape(self.wavelet_packet['a' * level].data)
+            dx = (self.bounds.right - self.bounds.left) / c
+            dy = (self.bounds.top - self.bounds.bottom) / r
+            rows, cols = np.where(sig_mask)
+            coords = [rowcol_to_xy(r, c, dx, dy) for r, c in zip(rows, cols)]
+            centers.extend(coords)
 
-        plot_cell(ax, self.root_cell)
+        centers = np.array(list(set(centers)))  # ensure no duplicates
 
-        ax.set_xlim(self.bounds.left, self.bounds.right)
-        ax.set_ylim(self.bounds.bottom, self.bounds.top)
-        ax.set_aspect('equal', 'box')
-        ax.set_xlabel('X (UTM)')
-        ax.set_ylabel('Y (UTM)')
-        ax.set_title('Wavelet Cells')
+        elevations = self.interpolate_elevations(centers)
+        # with rasterio.open(self.raster) as src:
+        #     elevations = [val[0] * 3 for val in src.sample(centers)]
 
-        plt.show()
+        return np.column_stack((centers, elevations))
 
-    def convert_to_mesh(self, elevation_nan_value=-999999.0, exaggeration=3):
-        points = self.extract_points(self.root_cell)
-        points = np.array(list(set(points)))
-
-        mask = self.points_within_original_dem(points)
-        points = points[mask]
-
-        elevations = self.extract_elevation_values(points)
-
-        mask = elevations != elevation_nan_value
-        elevations = elevations[mask]
-        points = points[mask]
-
-        points_3d = [(points[i, 0], points[i, 1], elevations[i] * exaggeration) for i in range(len(elevations))]
-
-        point_cloud = pv.PolyData(points_3d)
+    def convert_coords_to_mesh(self, coords):
+        point_cloud = pv.PolyData(coords)
         mesh = point_cloud.delaunay_2d()
-
         return mesh
 
-    def extract_points(self, cell):
-        points = []
+    def interpolate_elevations(self, points):
+        # Extract original DEM data and its transform parameters
+        original_data = self.data
+        original_transform = self.transform
+        original_height, original_width = original_data.shape
 
-        if self.cell_intersects_original_dem(cell):
-            points.extend([
-                (cell.ul_x, cell.ul_y),
-                (cell.ul_x + cell.dx, cell.ul_y),
-                (cell.ul_x, cell.ul_y - cell.dy),
-                (cell.ul_x + cell.dx, cell.ul_y - cell.dy)
-            ])
+        # Create coordinate grids from the original DEM
+        x = np.arange(original_width) * original_transform[0] + original_transform[2]
+        y = np.arange(original_height) * original_transform[4] + original_transform[5]
 
-            #points.append((cell.ul_x + cell.dx / 2, cell.ul_y - cell.dy / 2))
+        # Create meshgrid for interpolation
+        grid_x, grid_y = np.meshgrid(x, y)
+        grid_z = original_data.flatten()
 
-        # If this cell has children, recursively extract points from them
-        if cell.children:
-            for child in cell.children:
-                points.extend(self.extract_points(child))
+        # Flatten the grid coordinates for griddata
+        points_grid = np.vstack((grid_x.flatten(), grid_y.flatten())).T
 
-        return points
+        # Interpolate elevation for given points
+        elevations = griddata(points_grid, grid_z, points, method='linear')
 
-    def cell_intersects_original_dem(self, cell):
-        original_bounds = self.dem_original['bounds']
-        return (cell.ul_x < original_bounds.right and
-                cell.ul_x + cell.dx > original_bounds.left and
-                cell.ul_y > original_bounds.bottom and
-                cell.ul_y - cell.dy < original_bounds.top)
-
-    def points_within_original_dem(self, points):
-        original_bounds = self.dem_original['bounds']
-        mask = ((points[:, 0] >= original_bounds.left) &
-                (points[:, 0] <= original_bounds.right) &
-                (points[:, 1] >= original_bounds.bottom) &
-                (points[:, 1] <= original_bounds.top))
-        return mask
-
-    def extract_elevation_values(self, points):
-        coords = [(points[row, 0], points[row, 1]) for row in range(0, len(points))]
-
-        with rasterio.open(self.raster) as src:
-            elevations = [val[0] for val in src.sample(coords)]
-
-        return np.array(elevations)
+        return elevations
 
     @staticmethod
     def plot_mesh(mesh):
         plotter = pv.Plotter()
         plotter.add_mesh(mesh)
         plotter.show()
+
+    # def fill_tree(self, sig_masks_list):
+    #     xmin = self.bounds.left
+    #     ymax = self.bounds.top
+    #     self.root_cell.subdivide(sig_masks_list, xmin, ymax)
+    #
+    # def plot_cells(self, with_data=True):
+    #     fig, ax = plt.subplots()
+    #     if with_data:
+    #         ax.imshow(self.data, extent=self.get_extent())
+    #
+    #     def plot_cell(ax, cell, color='blue'):
+    #
+    #         rect = patches.Rectangle(
+    #             (cell.ul_x, cell.ul_y - cell.dy), cell.dx, cell.dy,
+    #             linewidth=1, edgecolor=color, facecolor='none'
+    #         )
+    #         ax.add_patch(rect)
+    #
+    #         for child in cell.children:
+    #             plot_cell(ax, child, color='red')
+    #
+    #     plot_cell(ax, self.root_cell)
+    #
+    #     ax.set_xlim(self.bounds.left, self.bounds.right)
+    #     ax.set_ylim(self.bounds.bottom, self.bounds.top)
+    #     ax.set_aspect('equal', 'box')
+    #     ax.set_xlabel('X (UTM)')
+    #     ax.set_ylabel('Y (UTM)')
+    #     ax.set_title('Wavelet Cells')
+    #
+    #     plt.show()
+    #
+    # def convert_to_mesh(self, elevation_nan_value=-999999.0, exaggeration=3):
+    #     points = self.extract_points(self.root_cell)
+    #     points = np.array(list(set(points)))
+    #
+    #     mask = self.points_within_original_dem(points)
+    #     points = points[mask]
+    #
+    #     elevations = self.extract_elevation_values(points)
+    #
+    #     mask = elevations != elevation_nan_value
+    #     elevations = elevations[mask]
+    #     points = points[mask]
+    #
+    #     points_3d = [(points[i, 0], points[i, 1], elevations[i] * exaggeration) for i in range(len(elevations))]
+    #
+    #     point_cloud = pv.PolyData(points_3d)
+    #     mesh = point_cloud.delaunay_2d()
+    #
+    #     return mesh
+    #
+    # def extract_points(self, cell):
+    #     points = []
+    #
+    #     if self.cell_intersects_original_dem(cell):
+    #         points.extend([
+    #             (cell.ul_x, cell.ul_y),
+    #             (cell.ul_x + cell.dx, cell.ul_y),
+    #             (cell.ul_x, cell.ul_y - cell.dy),
+    #             (cell.ul_x + cell.dx, cell.ul_y - cell.dy)
+    #         ])
+    #
+    #         #points.append((cell.ul_x + cell.dx / 2, cell.ul_y - cell.dy / 2))
+    #
+    #     # If this cell has children, recursively extract points from them
+    #     if cell.children:
+    #         for child in cell.children:
+    #             points.extend(self.extract_points(child))
+    #
+    #     return points
+    #
+    # def cell_intersects_original_dem(self, cell):
+    #     original_bounds = self.dem_original['bounds']
+    #     return (cell.ul_x < original_bounds.right and
+    #             cell.ul_x + cell.dx > original_bounds.left and
+    #             cell.ul_y > original_bounds.bottom and
+    #             cell.ul_y - cell.dy < original_bounds.top)
+    #
+    # def points_within_original_dem(self, points):
+    #     original_bounds = self.dem_original['bounds']
+    #     mask = ((points[:, 0] >= original_bounds.left) &
+    #             (points[:, 0] <= original_bounds.right) &
+    #             (points[:, 1] >= original_bounds.bottom) &
+    #             (points[:, 1] <= original_bounds.top))
+    #     return mask
+    #
+    # def extract_elevation_values(self, points):
+    #     coords = [(points[row, 0], points[row, 1]) for row in range(0, len(points))]
+    #
+    #     with rasterio.open(self.raster) as src:
+    #         elevations = [val[0] for val in src.sample(coords)]
+    #
+    #     return np.array(elevations)
+    #
+    # @staticmethod
+    # def plot_mesh(mesh):
+    #     plotter = pv.Plotter()
+    #     plotter.add_mesh(mesh)
+    #     plotter.show()
